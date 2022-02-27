61264d072a7883a3ff675e8dd2c0e6750d6e5e97
abdelatif8chairi <abdelatif8chairi@gmail.com>
2022-02-27 16:12:01 +0100

    Add store app to settings as well as the static directory

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -62,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processor.cart_total_amount',
             ],
         },
     },
@@ -115,9 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/store/static/'                                                       #static data path
 
+STATICFILES_DIRS = [                                                                # static data directory
+    os.path.join(BASE_DIR, 'store/static/store'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
+CART_SESSION_ID = 'cart'                                                            # cart session
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
