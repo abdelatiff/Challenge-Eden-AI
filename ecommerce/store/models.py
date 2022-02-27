30b129d72bd4630259cac4c412901154a840d6ae
abdelatif8chairi <abdelatif8chairi@gmail.com>
2022-02-27 19:56:46 +0100

    Add product databse to the app, and insert into the product table the possible item

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class product(models.Model):
+    name = models.CharField(max_length=30)
+    Reference = models.CharField(default='145',max_length=30)
+    image = models.FileField(upload_to='store/static/store/img')
+    price = models.FloatField()
+#    def save(self):
+#        self.file_name = self.Rep.split('//')[:-1]
+#        super().save(self)
+    def __str__(self):
+        return self.name
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+lst = [['Fromage','1','fromage.jpg',3.0],['Pain','2','pain.jpg',1.0],['Tomate','3','tomate.jpg',1.0]]
 def store(request):
-    return render(request, 'store.html')
+
+    queryset = product.objects.filter(name='fromage')
+
+    for i in lst:
+        prod = product()
+        prod.name = i[0]
+        prod.Reference = i[1]
+        prod.image = i[2]
+        prod.price = i[3]
+        prod.save()
+
+    qs = product.objects.all()
+    key_set = set()
+    delete_ids_list = []
+    for object in qs:
+        object_key = object.name  # photo_id here
+        if object_key in key_set:
+            delete_ids_list.append(object.id)
+        else:
+            key_set.add(object_key)
+    product.objects.filter(id__in=delete_ids_list).delete()
+    query_results = product.objects.all()
+    print(query_results)
+    context = {'query_results': query_results}
+    return render(request, 'store.html',context=context)
