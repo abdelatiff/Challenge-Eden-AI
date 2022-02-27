6fbb7701e54635e119e619903cb05e8b2beebd37
abdelatif8chairi <abdelatif8chairi@gmail.com>
2022-02-27 18:27:19 +0100

    Correct issues in redirection and some bugs in the templates

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import *
+
+from django.conf import settings
+from django.conf.urls.static import static
 app_name = 'store'
 urlpatterns = [
-    path('main/', main, name='main'),
+    path('', store, name='store'),
 ]
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def main(request):
-    return render(request, 'main.html')
+def store(request):
+    return render(request, 'store.html')
