b26e96fe2ca2cbbb1ab8b88bd3b120fb863c038a
abdelatif8chairi <abdelatif8chairi@gmail.com>
2022-02-28 20:26:56 +0100

    Add cart functionnalities as well as the cart informations

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -10,8 +10,6 @@ urlpatterns = [
     path('cart/item_clear/<int:id>/', item_clear, name='item_clear'),
     path('cart/item_increment/<int:id>/',
          item_increment, name='item_increment'),
-    path('cart/item_decrement/<int:id>/',
-         item_decrement, name='item_decrement'),
     path('cart/cart_clear/', cart_clear, name='cart_clear'),
     path('cart/cart_detail/', cart_detail, name='cart_detail'),
 ]
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -54,22 +54,17 @@ def item_increment(request, id):
     cart = Cart(request)
     Product = product.objects.get(id=id)
     cart.add(product=Product)
-    return redirect("/cart/cart_detail/")
+    return redirect("/")
 
 
 
-def item_decrement(request, id):
-    cart = Cart(request)
-    Product = product.objects.get(id=id)
-    cart.decrement(product=Product)
-    return redirect("/cart/cart_detail/")
 
 
 
 def cart_clear(request):
     cart = Cart(request)
     cart.clear()
-    return redirect("/cart/cart_detail/")
+    return redirect("/")
 
 
 
